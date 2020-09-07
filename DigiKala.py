a8e59cfb107731c28954b9488039f1272ebe3021
Hossein <hksafa@gmail.com>
2020-09-07 15:24:31 +0430

    commitDigi

--- DigiKala.py
+++ DigiKala.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Sep  5 16:05:49 2020
 
-@author: h.safa
-"""
 
 import pandas as pd
 import numpy as np
@@ -13,12 +8,14 @@ pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 
 
-dkProduct=pd.read_excel("C:/Users/h.safa/Downloads/Personal Files/digikala_dataset[www.camelcase.ir]/product.xlsx")
-dkOrders=pd.read_csv("C:/Users/h.safa/Downloads/Personal Files/digikala_dataset[www.camelcase.ir]/orders.csv")
+dkProduct=pd.read_excel("C:/Users/h.safa/Downloads/Personal Files/DataScience/DataScience/Data Sets/Digikala Dataset/product.xlsx")
+dkOrders=pd.read_csv("C:/Users/h.safa/Downloads/Personal Files/DataScience/DataScience/Data Sets/Digikala Dataset/orders.csv")
 
 dkProduct.head()
 dkProduct.shape
 dkProduct.describe(include='all')
 dkProduct.columns
 
+dkOrders.head()
+dkOrders.columns
 dkOrders.describe(include='all')
